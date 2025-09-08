0003ad7124b9aab5d3470626cf0c7e676d0342e7
Mohamed Diab <diab206@users.noreply.github.com>
2025-09-08 18:06:54 +0300

    set the app colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.indigo;
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color textDark = Colors.black87;
+  static const Color textLight = Colors.white;
+  static const Color success = Colors.green;
+  static const Color error = Colors.red;
+}
